9ba9018aa92d5ca037faaf2f66f5dd3a568dda94
alex.myrko <alex.myrko@gmail.com>
2020-12-13 20:36:07 +0200

    My first commit

--- src/ReadHelper.java
+++ src/ReadHelper.java
@@ -14,6 +14,17 @@ public class ReadHelper {
     // Should receive from console decimal number of operation and return Enum type of corresponding operation
     public static Operation askOperation() throws IOException {
         Operation operation = null;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter operation no.: ");
+        while(true) {
+            String text = reader.readLine();
+            try {
+                operation = Operation.values()[Integer.parseInt(text)];
+                return operation;
+            } catch (Exception e) {
+                System.out.println("Not exist !  Try again !");
+            }
+        }
     }
 
     // Should receive from console decimal id number, check and return if exist
