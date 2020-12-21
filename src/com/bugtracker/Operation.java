9d863db6102dba8aefa2ac938e1de234a9a30ca8
alexmyrko <alex.myrko@gmail.com>
2020-12-21 22:58:41 +0200

    test Operation commit

--- src/com/bugtracker/Operation.java
+++ src/com/bugtracker/Operation.java
@@ -1,25 +1,29 @@
 package com.bugtracker;
 
+import java.io.IOException;
+
 public enum Operation {
     LOGIN, REGISTER, CREATE, EDIT, VIEW, EXIT;
 
     // get logging / register operation (LOGIN , REGISTER) or exit
-    public static Operation getLoginOperationByOrdinal(int n){
-        switch (n){
-            case 1 :
-                return LOGIN;
-            case 2:
-                return REGISTER;
-            case 3:
-                return EXIT;
-            default:
-                throw new IllegalArgumentException();
+    public static Operation getLoginOperationByOrdinal() throws IOException {
+        while (true) {
+            try {
+                System.out.println("Enter:\n1 - to login\n2 - to register\n3 - to exit\n");
+                int loginMenuVariant = ReadHelper.readNumber();
+                switch (loginMenuVariant) {
+                    case 1:
+                        return LOGIN;
+                    case 2:
+                        return REGISTER;
+                    case 3:
+                        return EXIT;
+                    default:
+                        throw new IllegalArgumentException("Please, choose correct variant!");
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
-    }
-
-    // get one of routine Operations (CREATE, EDIT, VIEW) or exit
-    public static Operation getRoutineOperationByOrdinal(int n){
-        // TODO
-
     }
 }
