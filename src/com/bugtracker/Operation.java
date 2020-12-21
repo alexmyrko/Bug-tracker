73bbd3ede95420af83c8c05f36e2e4c8f60a966c
maksim-protsenko <protsenko.maksim.88@gmail.com>
2020-12-21 23:15:34 +0200

    Added recall of menu in case of incorrect input

--- src/com/bugtracker/Operation.java
+++ src/com/bugtracker/Operation.java
@@ -1,36 +1,53 @@
 package com.bugtracker;
 
+import java.io.IOException;
+
 public enum Operation {
     LOGIN, REGISTER, CREATE, EDIT, VIEW, EXIT;
 
     // get logging / register operation (LOGIN , REGISTER) or exit
-    public static Operation getLoginOperationByOrdinal(int loginMenuVariant) {
-        System.out.println("Enter:\n1 - to login\n2 - to register\n3 - to exit\n");
-        switch (loginMenuVariant) {
-            case 1:
-                return LOGIN;
-            case 2:
-                return REGISTER;
-            case 3:
-                return EXIT;
-            default:
-                throw new IllegalArgumentException("Please, choose correct variant!");
+    public static Operation getLoginOperationByOrdinal() throws IOException {
+        while (true) {
+            try {
+                System.out.println("Enter:\n1 - to login\n2 - to register\n3 - to exit\n");
+                int loginMenuVariant = ReadHelper.readNumber();
+                switch (loginMenuVariant) {
+                    case 1:
+                        return LOGIN;
+                    case 2:
+                        return REGISTER;
+                    case 3:
+                        return EXIT;
+                    default:
+                        throw new IllegalArgumentException("Please, choose correct variant!");
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
+
     // get one of routine Operations (CREATE, EDIT, VIEW) or exit
-    public static Operation getRoutineOperationByOrdinal(int mainMenuVariant) {
-        System.out.println("Enter:\n1 - to create a new ticket\n2 - to edit ticket\n3 - to view ticket\n4 - to exit\n");
-        switch (mainMenuVariant) {
-            case 1:
-                return CREATE;
-            case 2:
-                return EDIT;
-            case 3:
-                return VIEW;
-            case 4:
-                return EXIT;
-            default:
-                throw new IllegalArgumentException("Please, choose correct variant!");
+    public static Operation getRoutineOperationByOrdinal() throws IOException {
+        while (true) {
+            try {
+                System.out.println("Enter:\n1 - Create ticket\n2 - Edit ticket\n3 - View ticket\n4 - Exit");
+                int mainMenuVariant = ReadHelper.readNumber();
+                switch (mainMenuVariant) {
+                    case 1:
+                        return CREATE;
+                    case 2:
+                        return EDIT;
+                    case 3:
+                        return VIEW;
+                    case 4:
+                        return EXIT;
+                    default:
+                        throw new IllegalArgumentException("Please, choose correct variant!");
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
